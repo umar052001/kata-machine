7007b8608895882271f3213c934cb8e9a3868394
umar052001 <umar.111nawaz@gmail.com>
2025-08-13 22:29:02 +0500

    Twocrystal ball solved better then primeagen B-)

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,15 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
-
+    const amountJump = Math.floor(Math.sqrt(breaks.length))
+    let i = amountJump;
+    for (; i < breaks.length; i += amountJump) {
+        if (breaks[i]) {
+            break;
+        }
+    }
+    for (i; i >= amountJump; i--) {
+        if (breaks[i]) {
+            return i
+        }
+    }
+    return -1
 }
