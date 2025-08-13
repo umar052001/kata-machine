a9ed77dbadefd8c7907c10944e85fea428795875
umar052001 <umar.111nawaz@gmail.com>
2025-08-13 23:07:31 +0500

    done with bubble sort using array destructuring

--- src/day1/BubbleSort.ts
+++ src/day1/BubbleSort.ts
@@ -1,3 +1,9 @@
 export default function bubble_sort(arr: number[]): void {
-
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = i; j < arr.length; j++) {
+            if (arr[j] > arr[j + 1]) {
+                [arr[j],arr[j+1]] = [arr[j+1],arr[j]]
+            }
+        }
+    }
 }
