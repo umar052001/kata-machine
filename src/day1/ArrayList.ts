7ec2efe05d24df577786840b50b69f2581d648d1
umar052001 <umar.111nawaz@gmail.com>
2025-08-30 20:13:06 +0500

    complted arraylist but have to revisit it

--- src/day1/ArrayList.ts
+++ src/day1/ArrayList.ts
@@ -1,27 +1,67 @@
 export default class ArrayList<T> {
     public length: number;
+    private items: T[];
 
-    
-
-    constructor() {
+    constructor(capacity: number = 0) {
+        this.length = 0;
+        this.items = Array(capacity);
     }
 
     prepend(item: T): void {
+        for (let i = this.length; i > 0; i--) {
+            this.items[i] = this.items[i - 1];
+        }
+        this.items[0] = item;
+        this.length++;
+    }
 
-}
     insertAt(item: T, idx: number): void {
+        if (idx < 0 || idx > this.length) {
+            throw new RangeError("Index out of bounds");
+        }
 
-}
+        for (let i = this.length; i > idx; i--) {
+            this.items[i] = this.items[i - 1];
+        }
+
+        this.items[idx] = item;
+        this.length++;
+    }
+
     append(item: T): void {
+        this.items[this.length] = item;
+        this.length++;
+    }
 
-}
     remove(item: T): T | undefined {
+        const idx = this.items.indexOf(item);
+        if (idx === -1) {
+            return undefined;
+        }
+        return this.removeAt(idx);
+    }
 
-}
     get(idx: number): T | undefined {
+        if (idx < 0 || idx >= this.length) {
+            return undefined;
+        }
+        return this.items[idx];
+    }
 
-}
     removeAt(idx: number): T | undefined {
+        if (idx < 0 || idx >= this.length) {
+            return undefined;
+        }
 
-}
+        const removed = this.items[idx];
+
+        for (let i = idx; i < this.length - 1; i++) {
+            this.items[i] = this.items[i + 1];
+        }
+
+        this.length--;
+        this.items.length = this.length;
+
+        return removed;
+    }
 }
