6eb231d81363ea9d980843ab13934f4566075075
umar052001 <umar.111nawaz@gmail.com>
2025-08-30 05:49:40 +0500

    did stack (think gotta learn queues and stacks again and again idk why but this typescript is not getting into my small brain. I'm having attacks of imposter syndrome idk what I'm gonna do about it 😭

--- src/day1/Stack.ts
+++ src/day1/Stack.ts
@@ -1,18 +1,37 @@
+type Node<T> = {
+    value: T;
+    prev?: Node<T>;
+};
 export default class Stack<T> {
     public length: number;
-
-    
-
+    private head?: Node<T>;
     constructor() {
+        this.length = 0;
+        this.head = undefined;
     }
 
     push(item: T): void {
-
-}
+        this.length++;
+        const node = { value: item } as Node<T>;
+        if (!this.head) {
+            this.head = node;
+            return;
+        }
+        node.prev = this.head;
+        this.head = node;
+    }
     pop(): T | undefined {
-
-}
+        const head = this.head as Node<T>;
+        this.length = Math.max(0, this.length - 1);
+        if (this.length === 0) {
+            this.head = undefined;
+            return head?.value;
+        }
+        this.head = head?.prev;
+        return head?.value;
+    }
     peek(): T | undefined {
+        return this.head?.value;
+    }
+}
 
-}
-}
